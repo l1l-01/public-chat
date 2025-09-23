019b56dae0c979940bc2aef8e88f90d58a1446db
l1l <khalil23ar@gmail.com>
2025-09-23 21:23:09 +0100

    Adding dotenv, postgres and typeorm to the project

---
+++ server/src/data-source.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import "dotenv/config";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  synchronize: process.env.NODE_ENV === "development",
+  logging: false,
+  entities: [],
+  migrations: [],
+  subscribers: [],
+});
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,15 @@
 import express from "express";
+import { AppDataSource } from "./data-source.ts";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (_req, res) => {
-  res.send("Hello, Express + TypeScript + Yarn + ESM 🚀");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error);
+  });
