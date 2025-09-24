ee05c3d3198a1c41e73e3935b81eb2d4cc1ee18f
l1l <khalil23ar@gmail.com>
2025-09-24 20:18:10 +0100

    Removing dist from the project

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
-app.use("/api", userRoutes);
+app.use("/api/auth/", userRoutes);
 
 AppDataSource.initialize()
   .then(() => {
